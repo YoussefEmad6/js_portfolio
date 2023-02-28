36a5f5aac552d973ad056a96a7ee02fe2e2e7a56
youssef917 <o@gmail.com>
2023-02-28 17:16:33 +0200

    github icon and link

--- src/components/portfolio/showcase/index.jsx
+++ src/components/portfolio/showcase/index.jsx
@@ -1,45 +1,49 @@
 import React from "react";
 
+import { BsGithub } from "react-icons/bs";
 import Arrow from "../../shared/arrow";
 
 import "./style.scss";
 
 const Showcase = ({ data, transition }) => {
-    return (
-        <div className="projects-showcase">
-            {data.map((project) => (
-                <div
-                    className={`showcase-item ${
-                        transition === "zoomout"
-                            ? "zoomOut"
-                            : transition === "zoomin"
-                            ? "zoomIn"
-                            : ""
-                    }`}
-                    key={project.id}
+  return (
+    <div className="projects-showcase">
+      {data.map((project) => (
+        <div
+          className={`showcase-item ${
+            transition === "zoomout"
+              ? "zoomOut"
+              : transition === "zoomin"
+              ? "zoomIn"
+              : ""
+          }`}
+          key={project.id}
+        >
+          <div className="meta-content">
+            <h3>{project.name}</h3>
+            <div className="links">
+              <div className="go-to-cta">
+                <a
+                  href={project.herf}
+                  className="text"
+                  rel="noreferrer"
+                  target="_blank"
                 >
-                    <div className="meta-content">
-                        <h3>{project.name}</h3>
-                        <div className="go-to-cta">
-                            <a
-                                href={project.herf}
-                                className="text"
-                                rel="noreferrer"
-                                target="_blank"
-                            >
-                                Project Details
-                            </a>
-                            <Arrow />
-                        </div>
-                    </div>
-                    <img
-                        src={project.media.thumbnail}
-                        alt="hh"
-                    />
-                </div>
-            ))}
+                  Project link
+                </a>
+                <Arrow />
+              </div>
+              <a href={project.herf}
+                  className="text"
+                  rel="noreferrer"
+                  target="_blank"><BsGithub /></a>
+            </div>
+          </div>
+          <img src={project.media.thumbnail} alt="hh" />
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Showcase;
